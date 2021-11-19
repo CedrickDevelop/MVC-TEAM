c591e52338ffcd40dfa9e5ec8ffdf761f1fc6fb6
aimad bachar <aimadbacharpro@outlook.fr>
2021-11-19 15:59:49 +0100

    add var email

--- views/welcome.php
+++ views/welcome.php
@@ -1,3 +1,4 @@
 <h1>Welcome <?= $username ?></h1>
+<h1>Welcome <?= $email ?></h1>
 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro iure nam sunt maiores rem, aliquam unde corrupti assumenda accusamus eum voluptate ea quod mollitia tenetur soluta dolores reprehenderit explicabo tempore?</p>
 
